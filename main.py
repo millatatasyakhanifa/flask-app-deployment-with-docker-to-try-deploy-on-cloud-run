732f6a272b47dad82c46b17840635ad1cdbd2088
Millata Tasyakhanifa <79732219+millatatasyakhanifa@users.noreply.github.com>
2022-10-11 14:31:08 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-#import Flask 
+import os
+
 from flask import Flask
-#create an instance of Flask
+
 app = Flask(__name__)
-@app.route('/')
-def home():
-    return "Hello World"
-if __name__ == '__main__':
-    app.run(debug=True)
+
+
+@app.route("/")
+def hello_world():
+    name = os.environ.get("NAME", "World")
+    return "Hello {}!".format(name)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
